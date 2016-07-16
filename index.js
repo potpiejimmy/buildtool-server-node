9d4b4fa7ba0d071984200397e5dcb07fab839533
Thorsten <thorsten@potpiejimmy.de>
2016-07-16 17:47:06 +0200

    listen on localhost only

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ app.use(bodyParser.json());
 app.use('/buildtool/res/jobs', jobs);
 app.use('/buildtool/res/params', params);
 
-app.get('/', function (req, res) {
+app.get('/buildtool', function (req, res) {
   res.send('Resources at:<br/>/buildtool/res/jobs/&lt;unit&gt;/&lt;job&gt;<br/>/buildtool/res/params/&lt;unit&gt;/&lt;param&gt;');
 });
 
-app.listen(3002, function () {
-  console.log('Buildtool server listening on port 3002.');
+app.listen(3002, 'localhost', function () {
+  console.log('Buildtool server listening on port localhost:3002/buildtool');
 });
