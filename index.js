5d400d6f4dba10203ac3689db9d2f35a2b79f387
Thorsten <thorsten@potpiejimmy.de>
2016-07-27 15:03:58 +0200

    added support for /buildtool/update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var exec = require('child_process').exec;
 
 var db = require('./util/db');
 
@@ -16,6 +17,13 @@ app.use('/buildtool/res/params', params);
 
 app.get('/buildtool', function (req, res) {
   res.send('Resources at:<br/>/buildtool/res/jobs/&lt;unit&gt;/&lt;job&gt;<br/>/buildtool/res/params/&lt;unit&gt;/&lt;param&gt;');
+});
+
+app.get('/buildtool/update', function(req, res) {
+  exec('git pull', function(err, stdout, stderr) {
+    if (err) res.send(err);
+    else res.send(stdout);
+  });
 });
 
 app.listen(3002, 'localhost', function () {
